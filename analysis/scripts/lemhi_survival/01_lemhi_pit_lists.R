fcd1f44eec9557edeb9f601146521725f80886e2
Mike Ackerman <dirtpathwanderer@gmail.com>
2023-03-31 11:33:05 -0600

    Adding PIT Lists

--- analysis/scripts/lemhi_survival/01_lemhi_pit_lists.R
+++ analysis/scripts/lemhi_survival/01_lemhi_pit_lists.R
@@ -13,7 +13,7 @@ library(here)
 library(janitor)
 
 # read in tagging details
-lem_tag_deets = list.files(here("analysis/data/raw_data/tagging_details"), pattern = "*.csv", full.names = T) %>%
+lem_chnk_tag_deets = list.files(here("analysis/data/raw_data/tagging_details"), pattern = "*.csv", full.names = T) %>%
   map_df(~read_csv(., show_col_types = F, col_types = cols(
     `Species Code` = col_double(),
     `Run Code` = col_double()
@@ -21,48 +21,41 @@ lem_tag_deets = list.files(here("analysis/data/raw_data/tagging_details"), patte
   as_tibble() %>%
   clean_names() %>%
   # filter for just Chinook salmon and steelhead, e-fishing and screw traps
-  filter(species_name == c("Chinook", "Steelhead"),
-         capture_method_code == c("SHOCK", "SCREWT"),
-         # not many tags prior to 2003
-         mark_year_yyyy >= 2003,
+  filter(species_name == "Chinook",
+         capture_method_code == "SHOCK" | capture_method_code == "SCREWT",
          # filter out some very small or large sizes
          length_mm >= 50,
-         length_mm <= 250)
+         length_mm <= 200,
+         # no e-fishing prior to 2009
+         mark_year_yyyy >= 2009) %>%
+  mutate(juv_stage = case_when(
+    mark_month_number > 8 ~ "Presmolt",
+    mark_month_number < 7 ~ "Smolt",
+    between(mark_month_number, 7, 8) ~ "Parr"
+  )) %>%
+  mutate(brood_year_yyyy = if_else(juv_stage == "Smolt",
+                                   mark_year_yyyy - 2,
+                                   mark_year_yyyy - 1))
 
-tabyl(lem_tag_deets$mark_year_yyyy)
-tabyl(lem_tag_deets$length_mm)
+tabyl(lem_chnk_tag_deets, mark_year_yyyy, capture_method_code)
 
-# write tag lists to file (by mark year)
-lem_tag_deets %>%
-  split(list(.$mark_year_yyyy)) %>%
+# write tag lists to file, by capture method and brood year
+lem_chnk_tag_deets %>%
+  split(list(.$capture_method_code, .$brood_year_yyyy)) %>%
   map(.f = function(x) {
     x %>%
       select(tag_code) %>%
       distinct() %>%
       write_delim(file = here("analysis/data/raw_data/tag_lists/lem_surv",
-                              paste0("MY",
-                                     unique(x$mark_year_yyyy),
-                                     "_tags.txt")),
-                  delim = "\n",
-                  col_names = F)
-  })
-
-# write tag lists to file (by species & mark year)
-lem_tag_deets %>%
-  split(list(.$species_name, .$mark_year_yyyy)) %>%
-  map(.f = function(x) {
-    x %>%
-      select(tag_code) %>%
-      distinct() %>%
-      write_delim(file = here("analysis/data/raw_data/tag_lists/lem_surv",
-                              paste0(unique(x$species_name),
-                                     "_MY",
-                                     unique(x$mark_year_yyyy),
+                              paste0("BY",
+                                     unique(x$brood_year_yyyy),
+                                     "_",
+                                     unique(x$capture_method_code),
                                      "_tags.txt")),
                   delim = "\n",
                   col_names = F)
   })
 
 # save tagging details
-write_rds(lem_tag_deets,
+write_rds(lem_chnk_tag_deets,
           file = here('analysis/data/derived_data/lemhi_taggging_details.rds'))
