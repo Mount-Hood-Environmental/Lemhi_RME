24d774e66c14277437c8b292a746fc19eace099c
Mike Ackerman <dirtpathwanderer@gmail.com>
2023-03-31 13:47:49 -0600

    build config, nodes, read observations

--- analysis/scripts/lemhi_survival/01_lemhi_pit_lists.R
+++ analysis/scripts/lemhi_survival/01_lemhi_pit_lists.R
@@ -59,3 +59,67 @@ lem_chnk_tag_deets %>%
 # save tagging details
 # write_rds(lem_chnk_tag_deets,
 #           file = here('analysis/data/derived_data/lemhi_tagging_details.rds'))
+
+# load PITcleanr
+# remotes::install_github("mackerman44/PITcleanr@main")
+library(PITcleanr)
+
+# build a configuration file, recode and/or combine some sites
+config_file = buildConfig() %>%
+  mutate(node = site_code) %>%
+  # GRS = Lower Granite Dam Spillway
+  mutate(node = case_when(
+    site_code %in% c("GRS")                                ~ "GRJ", # Lower Granite Dam Spillway
+    site_code %in% c("LGRLDR")                             ~ "GRA", # LGR - Release into the Adult Fish Ladder
+    site_code %in% c("IHA", "IHR")                         ~ "ICH", # Ice Harbor Adult, Ice Harbor Dam
+    site_code %in% c("Mc1", "MC2")                          ~ "MCN", # McNary Oregon Shore Ladder, McNary Washington Shore Ladder,
+    site_code %in% c("JDALD1", "JDALD2")                   ~ "JO1", # Release into South Fish Ladder, Release into North Fish Ladder
+    site_code %in% c("TDA", "TD2")                         ~ "TD1", # The Dalles Dam, The Dalles North Fish Ladder
+    site_code %in% c("B2J", "BCC", "B1J", "BVX")           ~ "BOJ", # Combine all juvenile sites at Bonneville
+    site_code %in% c("BO1", "BO2", "BO3", "BO4", "BONAFF") ~ "BON", # Combine all adult sites at Bonneville
+    TRUE ~ node
+  ))
+
+# create parent-child table
+parent_child = tribble(~parent, ~child,
+                       "LEMHI", "LLR",
+                       "LLR", "GRJ",
+                       "GRJ", "GOJ",
+                       "GOJ", "LMJ",
+                       "LMJ", "ICH",
+                       "ICH", "MCJ",
+                       "MCJ", "JDJ",
+                       "JDJ", "BOJ",
+                       "BOJ", "BON",
+                       "BON", "TD1",
+                       "TD1", "JO1",
+                       "JO1", "MCN",
+                       "MCN", "ICH",
+                       "ICH", "LMA",
+                       "LMA", "GOA",
+                       "GOA", "GRA",
+                       "GRA", "USE")
+
+# plot parent-child table
+plotNodes(parent_child = parent_child)
+
+# get observation data from all brood years and both capture methods
+cases = expand.grid(2007:2021,
+                  c("SCREWT", "SHOCK")) %>%
+  mutate(case = paste0("BY",
+                       Var1,
+                       "_",
+                       Var2)) %>%
+  select(case) %>%
+  filter(case != "BY2007_SHOCK") %>%
+  as_tibble()
+
+obs_df = cases %>%
+  mutate(ptagis_raw = map(cases,
+                          .f = function(cs) {
+                            readCTH(here("analysis/data/raw_data/PTAGIS/lem_surv",
+                                         paste0(cs, ".csv")))
+                          }))
+
+
+
