c7f0b99e0b36c9bab6be506fb9d120ed312e7f7e
Mike Ackerman <dirtpathwanderer@gmail.com>
2023-04-26 16:55:39 -0600

    draft CJS

--- analysis/scripts/lemhi_survival/03_lemhi_cjs.R
+++ analysis/scripts/lemhi_survival/03_lemhi_cjs.R
@@ -15,50 +15,41 @@ library(marked)
 # read in capture histories, nodes of interest, etc.
 load("S:/main/data/fish/lem_surv/lem_survival.Rdata")
 
-# unnest all compressed observations
-comp_all = obs_df %>%
-  select(-ptagis_raw, -ch) %>%
-  unnest(comp)
+# adapting some code from https://jamesepaterson.github.io/jamespatersonblog/2020-04-26_introduction_to_CJS.html
 
-# unnest all capture histories
-ch_all = obs_df %>%
-  select(-ptagis_raw, -comp) %>%
-  unnest(ch) %>%
-  mutate(across(c(brood_year,
-                  capture_method),
-                as.factor))
-
-# basic CJS (constant survival and detection)
+# basic CJS (constant survival and detection)... this is nonsensical in our case, just seeing if it runs
 cjs.m1 = crm(obs_df$ch[[1]])
 
 # examine model and coefficient estimates
 cjs.m1
 plogis(cjs.m1$results$beta$Phi)
 
+# process capture history data frame for MARK analysis
+?process.data
+lemhi_proc = process.data(ch_all)
 
-#----------------------------------------------
-# adapted some code from https://jamesepaterson.github.io/jamespatersonblog/2020-04-26_introduction_to_CJS.html
+# create design dataframes for model
+?make.design.data
+lemhi_ddl = make.design.data(lemhi_proc)
 
-lemhi_proc <- process.data(ch_all)
+Phi.dot      = list(formula = ~ 1)
+Phi.cap      = list(formula = ~ capture_method)
+Phi.time     = list(formula = ~ time)
+Phi.yr       = list(formula = ~ brood_year)
+Phi.cap.time = list(formula = ~ capture_method * time)
 
-lemhi_ddl <- make.design.data(lemhi_proc)
+p.cap  = list(formula = ~ capture_method)
+p.time = list(formula = ~ time)
+p.yr   = list(formula = ~ brood_year)
 
-Phi.dot <- list(formula = ~ 1)
-Phi.cap <- list(formula = ~ capture_method)
-Phi.time <- list(formula = ~ time)
-Phi.yr <- list(formula = ~ brood_year)
-Phi.cap.time <- list(formula = ~ capture_method * time)
+?create.model.list
+cml = create.model.list(c("Phi","p")) # makes all possible combinations of those parameter formulas
+results = crm.wrapper(cml,
+                      data = lemhi_proc,
+                      ddl = lemhi_ddl,
+                      external = FALSE,
+                      accumulate = FALSE,
+                      hessian = TRUE)
 
-p.cap <- list(formula = ~ capture_method)
-p.time <- list(formula = ~ time)
-p.yr <- list(formula = ~ brood_year)
-
-cml <- create.model.list(c("Phi","p")) # makes all possibile combinations of those parameter formulas
-results <- crm.wrapper(cml,
-                       data = lemhi_proc,
-                       ddl = lemhi_ddl,
-                       external = FALSE,
-                       accumulate = FALSE,
-                       hessian = TRUE)
-
+# view results
 results
