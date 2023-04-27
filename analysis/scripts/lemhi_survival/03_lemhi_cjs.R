7a1cf0a65a5863a1d92c7f12160bd10eb4867ee6
Mike Ackerman <dirtpathwanderer@gmail.com>
2023-04-27 15:26:26 -0600

    added suggestion for collapsing nodes

--- analysis/scripts/lemhi_survival/03_lemhi_cjs.R
+++ analysis/scripts/lemhi_survival/03_lemhi_cjs.R
@@ -15,18 +15,37 @@ library(marked)
 # read in capture histories, nodes of interest, etc.
 load("S:/main/data/fish/lem_surv/lem_survival.Rdata")
 
+# some suggested code for collapsing some sites
+chmat = splitCH(ch_all$ch) %>%
+  as_tibble()
+names(chmat) = nodes_of_interest
+
+chmat = chmat %>%
+  mutate(U_TRIBS = LLSPRC + BIGSPC + CANY2C + `18MILC` + BIG8MC + LEEC + HAYDNC,
+         U_TRAPS = LEMTRP + HYDTRP) %>%
+  # our new capture history nodes
+  select(U_TRIBS, U_TRAPS, LLRTP, LLR, GRJ, BLW_GRJ) %>%
+  as.matrix() %>%
+  collapseCH()
+
+ch_new = ch_all %>%
+  mutate(ch = chmat) %>%
+  # filter out some errant capture histories
+  filter(ch != "000000") %>%
+  filter(!grepl(2, ch))
+
 # adapting some code from https://jamesepaterson.github.io/jamespatersonblog/2020-04-26_introduction_to_CJS.html
 
 # basic CJS (constant survival and detection)... this is nonsensical in our case, just seeing if it runs
-cjs.m1 = crm(obs_df$ch[[1]])
+#cjs.m1 = crm(ch_new)
 
 # examine model and coefficient estimates
-cjs.m1
-plogis(cjs.m1$results$beta$Phi)
+#cjs.m1
+#plogis(cjs.m1$results$beta$Phi)
 
 # process capture history data frame for MARK analysis
 ?process.data
-lemhi_proc = process.data(ch_all)
+lemhi_proc = process.data(ch_new)
 
 # create design dataframes for model
 ?make.design.data
@@ -35,7 +54,7 @@ lemhi_ddl = make.design.data(lemhi_proc)
 Phi.dot      = list(formula = ~ 1)
 Phi.cap      = list(formula = ~ capture_method)
 Phi.time     = list(formula = ~ time)
-Phi.yr       = list(formula = ~ brood_year)
+Phi.by       = list(formula = ~ brood_year)
 Phi.cap.time = list(formula = ~ capture_method * time)
 
 p.cap  = list(formula = ~ capture_method)
@@ -53,7 +72,6 @@ results = crm.wrapper(cml,
 
 # view results
 results
-top_mod_results = results$Phi.yr.p.time
 
 # save results
 save(results,
@@ -61,4 +79,4 @@ save(results,
      file = "S:/main/data/fish/lem_surv/lem_surv_results.Rdata")
 
 # load, if needed
-load("S:/main/data/fish/lem_surv/lem_surv_results.Rdata")
+# load("S:/main/data/fish/lem_surv/lem_surv_results.Rdata")
